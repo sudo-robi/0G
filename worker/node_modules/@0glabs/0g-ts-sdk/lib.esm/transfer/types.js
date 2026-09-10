b83fd9a6e60d5a353cdbd8e5b2d8343f3d12a34c
Robi <anierobipaschal70@gmail.com>
2026-09-10 17:34:59 +0100

    fix: remove node_modules and build artifacts from tracking

--- worker/node_modules/@0glabs/0g-ts-sdk/lib.esm/transfer/types.js
+++
@@ -1,9 +0,0 @@
-export var defaultUploadOption = {
-    tags: '0x',
-    finalityRequired: true,
-    taskSize: 1,
-    expectedReplica: 1,
-    skipTx: false,
-    fee: BigInt(0),
-};
-//# sourceMappingURL=types.js.map
